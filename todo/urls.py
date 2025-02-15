9aa06cc3be6ad4460e4a12c1cec4e97d5a5adb11
Le'Mashon <Leonardlemashon@gmail.com>
2025-02-16 00:12:59 +0300

    Update urls.py

--- todo/urls.py
+++ todo/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from .views import home, remove
 
-url_patterns = [
-    path('', home, name='todo'),
+urlpatterns = [
+    path('todo/', home, name='todo'), 
     path('del/<str:item_id>/', remove, name='del'),
 ]
